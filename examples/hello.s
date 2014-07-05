f4bac8ee54aeb85f385d8da135a93f6798c2367e
md_5 <git@md-5.net>
2014-07-05 14:27:33 +1000

    Add example files hello and hello.s.
    These files simply print "Hello World\n" to stdout when run. They are linked statically against ulibc. Our goal will be to successfully run these files by implementing the necessary compilation and system calls.

---
+++ examples/hello.s
@@ -0,0 +1,17 @@
+.data
+	hello:
+		.asciz		"Hello, world!\n"
+	length:
+		.word		. - hello
+
+.global main
+
+.text
+	main: 
+		move	$4, $0			# load stdout fd
+		la		$5, hello		# load string address
+		lw		$6, length		# load string length
+		li		$2, 4004		# specify system write service
+		syscall					# call the kernel (write string)
+		li		$2, 0			# load return code
+		j		$31				# return to caller
